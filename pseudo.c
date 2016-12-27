d321b28c68f56f1057b39dde08d428c8b1e49526
Dan Houldsworth <dan@bramble.io>
2016-12-27 20:40:57 +0000

    Added links to possible simonK commits of interest - faster braking / faster timing / RebBrick70A

--- pseudo.c
+++ pseudo.c
@@ -18,3 +18,7 @@
 // * Also will understand it better
 
 
+// Check out :
+// better braking https://github.com/sim-/tgy/commit/d7f80f6e2242c6232fc88de376e5c3637b3e020e
+// Faster timing (16bit vs 24bit timing for 25000rpm+) https://github.com/sim-/tgy/commit/103edb5a08571cd4b7ccf0be5427eb3065666533
+// RedBrick 70A (PortD fets) https://github.com/sim-/tgy/commit/2d790fba39122ac8b296402fdb17ad8ba102288f
