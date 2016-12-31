e3777d2447185332a04707fc05aab71be1adf769
Dan Houldsworth <dan@kew.io>
2016-12-31 13:53:42 +0000

    Tested single FET flag and inline macros

--- source/main.asm
+++ source/main.asm
@@ -2,12 +2,33 @@
 ; simpleSimonK - Super simple ESC firmware
 ;--------------------------------------------------------------------------
 
-;-- Device ----------------------------------------------------------------
-.include "m8def.inc"
+; Load 2-byte immediate
+.macro ldi2
+		ldi	@0, byte1(@2)
+		ldi	@1, byte2(@2)
+.endmacro
+
+; Load 3-byte immediate
+.macro ldi3
+		ldi	@0, byte1(@3)
+		ldi	@1, byte2(@3)
+		ldi	@2, byte3(@3)
+.endmacro
+
+; Store immediate in SRAM
+.macro stsi
+	ldi	temp1, @1
+	sts	@0, temp1
+.endmacro
+
+; Immediate out to any port via temp1 (all reg on mega8 are <0x3f)
+.macro outi
+	ldi	temp1, @1
+	out	@0, temp1
+.endmacro
+
 ;-- Declarations ----------------------------------------------------------
 .include "header.inc"
-;-- Instruction extension macros ------------------------------------------
-.include "macros.inc"
 ;-- SRAM definitions / DSEG -----------------------------------------------
 .include "sram.inc"
 ;-- Interupt table & service routines -------------------------------------
