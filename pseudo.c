966bfab783bb818e1c318a79c6fe9853c7af2668
Dan Houldsworth <dan@kew.io>
2016-12-31 10:43:56 +0000

    Stripped beep. ADWIX macro

--- pseudo.c
+++ pseudo.c
@@ -22,3 +22,16 @@
 // better braking https://github.com/sim-/tgy/commit/d7f80f6e2242c6232fc88de376e5c3637b3e020e
 // Faster timing (16bit vs 24bit timing for 25000rpm+) https://github.com/sim-/tgy/commit/103edb5a08571cd4b7ccf0be5427eb3065666533
 // RedBrick 70A (PortD fets) https://github.com/sim-/tgy/commit/2d790fba39122ac8b296402fdb17ad8ba102288f
+
+
+/*
+BEEP:
+ON  : 16us
+OFF :
+	200/2  *  16 (=1120 : ~1.5% duty)   REPEAT  80 times == 129k
+	180/2  *  16 (=1120 : ~1.5% duty)   REPEAT 100 times == 145k
+	160/2  *  16 (=1120 : ~1.5% duty)   REPEAT 120 times == 155k
+	140/2  *  16 (=1120 : ~1.5% duty)   REPEAT 140 times == 159k
+
+Not entirely equal durations...
+*/
